b149b16c43335b6b9e78ad070893e17edad8ccdd
youngsil <kys4548@gmail.com>
2020-03-28 18:59:17 +0900

    2020-03-28

---
+++ springboot/springboot10/src/test/java/com/example/springboot10/SampleControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.springboot10;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WebClient webClient;
+
+//    @Test
+//    public void hello() throws Exception {
+//        mockMvc.perform(get("/hello"))
+//                .andExpect(status().isOk())
+//                .andDo(print())
+//                .andExpect(view().name("hello"))
+//                .andExpect(model().attribute("name", is("youngsil")))
+//                .andExpect(content().string(containsString("youngsil")));
+//    }
+
+    @Test
+    public void hello() throws IOException {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("youngsil");
+
+
+    }
+
+}
---
+++ springboot/springboot11/src/main/java/com/example/springboot11/SampleException.java
@@ -0,0 +1,4 @@
+package com.example.springboot11;
+
+public class SampleException extends  RuntimeException{
+}
