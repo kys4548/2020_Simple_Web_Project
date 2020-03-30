abd5ca70d5a669d01ce2ba033030bb9ea7b382f3
youngsil <kys4548@gmail.com>
2020-03-30 22:23:53 +0900

    POST /api/events returns 201

---
+++ springboot/rest-api-with-spring/src/main/java/com/example/restapiwithspring/events/EventController.java
@@ -0,0 +1,30 @@
+package com.example.restapiwithspring.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        Event newEvent = this.eventRepository.save(event);
+        URI uri = ControllerLinkBuilder.linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        event.setId(10);
+        return ResponseEntity.created(uri).body(event);
+    }
+}
--- springboot/rest-api-with-spring/src/test/java/com/example/restapiwithspring/events/EventControllerTest.java
+++ springboot/rest-api-with-spring/src/test/java/com/example/restapiwithspring/events/EventControllerTest.java
@@ -1,15 +1,22 @@
 package com.example.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,12 +26,34 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                            .name("Spring")
+                            .description("REST API Development with Spring")
+                            .beginEnrollmentDateTime(LocalDateTime.of(2020, 3, 30, 21, 58))
+                            .closeEnrollmentDateTime(LocalDateTime.of(2020, 3, 31, 21, 58))
+                            .beginEventDateTime(LocalDateTime.of(2020, 4, 1, 21, 58))
+                            .endEventDateTime(LocalDateTime.of(2020,4,2,21,58))
+                            .basePrice(100)
+                            .maxPrice(200)
+                            .limitOfEnrollment(100)
+                            .location("강남역 D2 스타텁 팩토리")
+                            .build();
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());  //201
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated()) //201
+                .andExpect(jsonPath("id").exists());
     }
 
 }
