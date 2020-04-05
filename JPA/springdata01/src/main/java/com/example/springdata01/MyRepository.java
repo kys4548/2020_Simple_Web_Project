af25f5035bcbfd1ab337c6869b4e1c0c68e4fc10
youngsil <kys4548@gmail.com>
2020-04-06 05:33:51 +0900

    springdata common : null handling

--- JPA/springdata01/src/main/java/com/example/springdata01/MyRepository.java
+++ JPA/springdata01/src/main/java/com/example/springdata01/MyRepository.java
@@ -2,13 +2,21 @@ package com.example.springdata01;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface MyRepository<T, Id extends Serializable> extends Repository<T, Id> {
-    <E extends T> E save(E entity);
+    <E extends T> E save(@NonNull E entity);
 
     List<T> findAll();
+
+    long count();
+
+    @Nullable
+    <E extends T> Optional<E> findById(Id id);
 }
--- JPA/springdata01/src/test/java/com/example/springdata01/CommentRepositoryTest.java
+++ JPA/springdata01/src/test/java/com/example/springdata01/CommentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +27,12 @@ class CommentRepositoryTest {
 
         List<Comment> all = commentRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
+
+        long count = commentRepository.count();
+        assertThat(count).isEqualTo(1);
+
+        Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId).isEmpty();
+        Comment comment1 = byId.orElse(new Comment());
     }
 }
