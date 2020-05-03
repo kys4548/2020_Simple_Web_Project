c3970e4b84b1a8630cda3e474063d1dacd2b62ee
youngsil <kys4548@gmail.com>
2020-05-03 18:26:26 +0900

    break for a moment

--- jpashop-springboot,jpa/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop-springboot,jpa/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @SpringBootApplication
 public class JpashopApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
--- jpashop-springboot,jpa/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop-springboot,jpa/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,16 +1,30 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpashopApplicationTests {
 
+	@PersistenceContext
+	EntityManager em;
+
 	@Test
+	@Transactional
+	@Rollback(false)
 	public void contextLoads() {
+
 	}
 
 }
