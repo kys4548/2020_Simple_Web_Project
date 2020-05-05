ac794d0e6081af6e8c5388b651ff7a041078b45e
youngsil <kys4548@gmail.com>
2020-05-05 23:29:20 +0900

    2020-05-05

--- jpashop-springboot,jpa/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop-springboot,jpa/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -50,6 +50,6 @@ public class OrderItem {
      * 주문상품 전체 가격 조회
      */
     public int getTotalPrice() {
-        return getOrderPrice() + getCount();
+        return getOrderPrice() * getCount();
     }
 }
--- jpashop-springboot,jpa/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop-springboot,jpa/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,6 +21,5 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
 
 }
