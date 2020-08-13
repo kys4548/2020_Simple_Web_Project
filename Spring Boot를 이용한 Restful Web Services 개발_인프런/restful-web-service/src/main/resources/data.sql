73c2f5e1e77f6cdcaa08bb1fd034c5771110d1bb
youngsil <kys4548@gmail.com>
2020-08-13 18:38:14 +0900

    Add create events test

--- Spring Boot를 이용한 Restful Web Services 개발_인프런/restful-web-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user  values (90001, sysdate(), '1a', '1b', '1c');
-insert into user  values (90002, sysdate(), '1a', '1b', '1c');
-insert into user  values (90003, sysdate(), '1a', '1b', '1c');
