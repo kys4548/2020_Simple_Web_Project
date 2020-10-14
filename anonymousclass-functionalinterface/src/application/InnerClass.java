1f532348945203fee5d60eab70605fe8d8c3a667
youngsil <kys4548@gmail.com>
2020-10-14 16:02:33 +0900

    innerclass and lambda

---
+++ anonymousclass-functionalinterface/src/application/InnerClass.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.function.IntBinaryOperator;
+
+public class InnerClass {
+    public static void main(String[] args) {
+        IntBinaryOperator add = new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int left, int right) {
+                return left + right;
+            }
+        };
+    }
+}
---
+++ anonymousclass-functionalinterface/src/application/Lambda.java
@@ -0,0 +1,9 @@
+package application;
+
+import java.util.function.IntBinaryOperator;
+
+public class Lambda {
+    public static void main(String[] args) {
+        IntBinaryOperator add = (a,b) -> a+b;
+    }
+}
