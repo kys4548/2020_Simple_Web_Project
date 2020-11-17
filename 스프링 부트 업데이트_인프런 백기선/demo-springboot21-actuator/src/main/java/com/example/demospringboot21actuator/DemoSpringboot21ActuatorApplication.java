8e068af2c1ad0893e0fa8f7a2b6cbfedf218f9f1
youngsil <kys4548@gmail.com>
2020-11-18 00:16:22 +0900

    study spring-boot2.2

---
+++ 스프링 부트 업데이트_인프런 백기선/demo-springboot21-actuator/src/main/java/com/example/demospringboot21actuator/DemoSpringboot21ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot21actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot21ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot21ActuatorApplication.class, args);
+    }
+
+}
---
+++ 스프링 부트 업데이트_인프런 백기선/demo-springboot21-actuator/src/test/java/com/example/demospringboot21actuator/DemoSpringboot21ActuatorApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.demospringboot21actuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringboot21ActuatorApplicationTest {
+
+    @Test
+    public void hello() {
+
+    }
+}
